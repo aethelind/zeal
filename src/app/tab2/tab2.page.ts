7c589a50093bee537601e503e999f198b23ccbd6
aethelind <aethelind@gmail.com>
2019-07-22 12:51:13 -0400

    added functions for match demonstration

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  selector: "app-tab2",
+  templateUrl: "tab2.page.html",
+  styleUrls: ["tab2.page.scss"]
 })
 export class Tab2Page {
-
   constructor() {}
 
+  async revealSocials() {
+    console.log("reveal now");
+    var s = document.getElementById("socials");
+    var l = document.getElementById("like");
+    s.style.display = "block";
+    l.style.display = "none";
+    //hide like
+    // reveal socials
+  }
+
+  async remove() {
+    console.log("remove dan");
+    var d = document.getElementById("dan");
+    d.style.display = "none";
+    //hide dan
+  }
+
+  async remove2() {
+    console.log("remove girl");
+    var d = document.getElementById("girl");
+    d.style.display = "none";
+    //hide dan
+  }
 }
